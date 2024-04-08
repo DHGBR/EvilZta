44fa98c3fcc3fc2826d11c6305c8981ada8ffc9c
DHGBR <166017538+DHGBR@users.noreply.github.com>
2024-04-08 14:38:05 -0300

    novas mudanças

--- EvilZeta.py
+++ EvilZeta.py
@@ -1,39 +1,80 @@
+import threading
 import shodan
+import coloredlogs, logging
 from colorama import just_fix_windows_console
 from termcolor import colored
 
 import subprocess
 import datetime
+from time import sleep
 import os
 
 
-# use Colorama to make Termcolor work on Windows too
-just_fix_windows_console()
+# Create a logger object.
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger)
 
+# # use Colorama to make Termcolor work on Windows too
+just_fix_windows_console()
 # --------------------------------------------------------
+# Salva uma nova linha no arquivo de texto
+def save_line(file_name, line):
+    # abrindo arquivo de texto
+    opened_file = open(file_name, 'a')
+    # escrevendo uma nova linha
+    opened_file.write(f"{line}\n")
+    # fechando o arquivo de texto
+    opened_file.close()
+# --------------------------------------------------------
+# --------------------------------------------------------
+# Lista de alvos vulneraveis
+targets = []
+
 # Funçao que checa a conexao de cada alvo
-def check_connection(ip):
-    process = subprocess.check_output(f'adb connect {ip} ', shell=True)
+def check_connection(target):
+    process = subprocess.check_output(f'adb connect {target} ', shell=True)
     date = datetime.datetime.now()
+
     if "cannot connect" in process.decode('utf-8'):
-        print (f"[!] Debug: {date} - Dispositivo  nao vulneravel: {ip}\n")
+       logger.info(f" - Dispositivo  nao vulneravel: {target}\n")
     else:
-        print (f"[*] Debug: {date} - Dispositivo Vulneravel\n")
-        check = subprocess.check_output(f'adb disconect {ip}', shell=True)
+        logger.info(f" - Dispositivo Vulneravel\n")
+
+        # Adicionando alvo a lista
+        targets.append(target)
+
+        # Salvando alvos
+        print (f"[!] Debug: {date} - Dispositivo  vulneravel salvo em targets.txt: {target}\n")
+        file_name = 'targets.txt'
+        save_line(file_name, target)
+
+        # Desconectando o dispositivo para testar o proximo
+        check = subprocess.check_output(f'adb disconnect {target}', shell=True)
         result = check.decode('utf-8')
-        print (f"[*] Debug: {date} - {result}\n")
+        logger.error(f" - {result}\n")
+
+    # Exibe a quantidade de alvos falhos salvos . 
+    logger.info(f"{len(targets)} dispositivos vulneraveis e salvos.")
+    # Aguarda 4 segundos para limpar a tela e voltar ao menu.
+    sleep(5)
+    os.system('cls')
 # --------------------------------------------------------
 
 
 # --------------------------------------------------------
 # Insira sua chave de API do Shodan
-API_KEY = ""
+# Alvo teste
+# 138.204.63.130
+API_KEY = "15hlnqlCHZTxvko4O5mJKM51AhyOLCSZ"
+
 def search_targets():
+    date = datetime.datetime.now()
     # Cria uma instância da classe API do Shodan
     api = shodan.Shodan(API_KEY)
 
     try:
-        # Realiza a consulta
+        # Realiza a consulta no shoda usando dork par "ADB"
+        # Filtro por pais Exp:  country:"BR"
         query = '"Android Debug Bridge" "Device" port:5555 country:"BR"'
         results = api.search(query)
 
@@ -41,10 +82,12 @@ def search_targets():
         print('Resultados encontrados:', results['total'])
         for result in results['matches']:
             target = result['ip_str']
-            print(f"IP: {target}")
+            print(f"[*] Debug: {date} - Testando: {target} ...\n")
+            # Checa alvo por alvo 
+            check_connection(target)
 
     except shodan.APIError as e:
-        print('Erro: %s' % e)
+       logger.info(f" Erro: {e}\n")
 # --------------------------------------------------------
 
 # --------------------------------------------------------
@@ -63,26 +106,25 @@ menu = """
     2 = Avaliar alvo específico.
        """
 # --------------------------------------------------------
-# Ip dos alvos 
-ip = "138.204.63.130"
+
 
 # Funçao principal
 def main():
     while True:
 
-        print(colored(f"{banner}\n", 'magenta'))
+        print(colored(f"{banner}\n", "magenta"))
 
-        print(colored(f"{menu}\n", 'green'))
+        print(colored(f"{menu}\n", "green"))
         
-        command = input(colored(">>", 'green'))
+        command = input(colored(">>", "green"))
         command = int(command)
         if command == 1:
             search_targets()
         if command == 2:
-            target = input("Seu Alvo: ")
+            target = input(colored("Seu Alvo:", "yellow"))
             check_connection(target)
         else:
-            print (colored("[!]Debug: Comando invalido", 'red'))
+            print ("[!]Debug: Comando invalido")
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # EvilZta
 Explora vulnerabilidade em dispositivos Android com depuração WiFi ativada, permitindo controle remoto via ADB. Objetiva compreender, desenvolver ferramentas e conscientizar sobre o risco. Colaboração com a comunidade de segurança cibernética.
+
+
+
+
+
+## 💻 Pré-requisitos
+
+Antes de começar, verifique se você atendeu aos seguintes requisitos:
+
+- Python 3*
+
+
+## ☕ Usando EvilZta
+
+Para usar o EvilZta, siga estas etapas:
+
+Linux e macOS:
+
+```
+pip install -r requirements.txt
+python3 EvilZeta.py
+```
+
+Windows:
+
+```
+pip install -r requirements.txt
+python3 EvilZeta.py
+```
+
+
+Adicione comandos de execução e exemplos que você acha que os usuários acharão úteis. Fornece uma referência de opções para pontos de bônus!
+
+## 📫 Contribuindo para <nome_do_projeto>
+
+Para contribuir com <nome_do_projeto>, siga estas etapas:
+
+1. Bifurque este repositório.
+2. Crie um branch: `git checkout -b <nome_branch>`.
+3. Faça suas alterações e confirme-as: `git commit -m '<mensagem_commit>'`
+4. Envie para o branch original: `git push origin <nome_do_projeto> / <local>`
+5. Crie a solicitação de pull.
+
+Como alternativa, consulte a documentação do GitHub em [como criar uma solicitação pull](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## 🤝 Colaboradores
+
+Agradecemos às seguintes pessoas que contribuíram para este projeto:
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="#" title="defina o titulo do link">
+        <img src="https://avatars3.githubusercontent.com/u/31936044" width="100px;" alt="Foto do Iuri Silva no GitHub"/><br>
+        <sub>
+          <b>Iuri Silva</b>
+        </sub>
+      </a>
+    </td>
+    <td align="center">
+      <a href="#" title="defina o titulo do link">
+        <img src="https://s2.glbimg.com/FUcw2usZfSTL6yCCGj3L3v3SpJ8=/smart/e.glbimg.com/og/ed/f/original/2019/04/25/zuckerberg_podcast.jpg" width="100px;" alt="Foto do Mark Zuckerberg"/><br>
+        <sub>
+          <b>Mark Zuckerberg</b>
+        </sub>
+      </a>
+    </td>
+    <td align="center">
+      <a href="#" title="defina o titulo do link">
+        <img src="https://miro.medium.com/max/360/0*1SkS3mSorArvY9kS.jpg" width="100px;" alt="Foto do Steve Jobs"/><br>
+        <sub>
+          <b>Steve Jobs</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
+
+## 😄 Seja um dos contribuidores
+
+Quer fazer parte desse projeto? Clique [AQUI](CONTRIBUTING.md) e leia como contribuir.
+
+## 📝 Licença
+
+Esse projeto está sob licença. Veja o arquivo [LICENÇA](LICENSE.md) para mais detalhes.
